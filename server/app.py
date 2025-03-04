a73e635a548384b1766a5f5b8b24b22ea45284b2
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2025-03-04 21:16:53 +0300

    bakery get api

--- server/app.py
+++ server/app.py
@@ -20,19 +20,28 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries =[]
+    for bakery in Bakery.query.all():
+        bakeries.append(bakery.to_dict())
+    return make_response(jsonify(bakeries), 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    
+    return make_response(jsonify(bakery.to_dict()), 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = []
+    for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all():
+        baked_goods.append(baked_good.to_dict())
+    return make_response(jsonify(baked_goods), 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    return make_response(most_expensive.to_dict(), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
